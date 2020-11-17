90eacd2ad01124831892c40ec49096f7cecd01af
Amar Yudhika <74227481+AmarCyber@users.noreply.github.com>
2020-11-17 15:25:02 +0700

    Add files via upload

---
+++ LINEPY/__init__.py
@@ -0,0 +1,13 @@
+from .client import LineClient
+from .channel import LineChannel
+from .poll import LinePoll
+from akad.ttypes import OpType
+
+__copyright__       = 'Copyright 2017 by Amar Yudhika'
+__version__         = '2.0.2'
+__license__         = 'BSD-3-Clause'
+__author__          = 'Amar Yudhika'
+__author_email__    = 'amaryudhika0@gmail.com'
+__url__             = 'https://yudhiprb.xtgem.com''
+
+__all__ = ['LineClient', 'LineChannel', 'LinePoll', 'OpType']
